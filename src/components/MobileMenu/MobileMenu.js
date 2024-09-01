19c6fba9ab7864be7a9bae399411ca2be6e86880
Feriansyah <devferiansyah@gmail.com>
2024-09-01 19:53:00 +0800

    feat: modal enter animation

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,40 +1,59 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from 'react'
+import styled, { keyframes } from 'styled-components/macro'
+import { DialogOverlay, DialogContent } from '@reach/dialog'
 
-import { QUERIES, WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants'
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from '../UnstyledButton'
+import Icon from '../Icon'
+import VisuallyHidden from '../VisuallyHidden'
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   return (
     <Overlay isOpen={isOpen} onDismiss={onDismiss}>
       <Content aria-label="Menu">
-        <CloseButton onClick={onDismiss}>
-          <Icon id="close" />
-          <VisuallyHidden>Dismiss menu</VisuallyHidden>
-        </CloseButton>
-        <Filler />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Footer>
-          <SubLink href="/terms">Terms and Conditions</SubLink>
-          <SubLink href="/privacy">Privacy Policy</SubLink>
-          <SubLink href="/contact">Contact Us</SubLink>
-        </Footer>
+        <Wrapper>
+          <CloseButton onClick={onDismiss}>
+            <Icon id="close" />
+            <VisuallyHidden>Dismiss menu</VisuallyHidden>
+          </CloseButton>
+          <Filler />
+          <Nav>
+            <NavLink href="/sale">Sale</NavLink>
+            <NavLink href="/new">New&nbsp;Releases</NavLink>
+            <NavLink href="/men">Men</NavLink>
+            <NavLink href="/women">Women</NavLink>
+            <NavLink href="/kids">Kids</NavLink>
+            <NavLink href="/collections">Collections</NavLink>
+          </Nav>
+          <Footer>
+            <SubLink href="/terms">Terms and Conditions</SubLink>
+            <SubLink href="/privacy">Privacy Policy</SubLink>
+            <SubLink href="/contact">Contact Us</SubLink>
+          </Footer>
+        </Wrapper>
       </Content>
     </Overlay>
-  );
-};
+  )
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+const slideIn = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+`
 
 const Overlay = styled(DialogOverlay)`
   position: fixed;
@@ -45,29 +64,41 @@ const Overlay = styled(DialogOverlay)`
   background: var(--color-backdrop);
   display: flex;
   justify-content: flex-end;
-`;
+  animation: ${fadeIn} 500ms;
+`
 
 const Content = styled(DialogContent)`
   background: white;
   width: 300px;
   height: 100%;
   padding: 24px 32px;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${slideIn} 500ms cubic-bezier(0.07, 0.83, 0.49, 1) backwards;
+    animation-delay: 200ms;
+  }
+`
+
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-`;
+  height: 100%;
+  animation: ${fadeIn} 600ms ease-out both;
+  animation-delay: 400ms;
+`
 
 const CloseButton = styled(UnstyledButton)`
   position: absolute;
   top: 10px;
   right: 0;
   padding: 16px;
-`;
+`
 
 const Nav = styled.nav`
   display: flex;
   flex-direction: column;
   gap: 16px;
-`;
+`
 
 const NavLink = styled.a`
   color: var(--color-gray-900);
@@ -79,23 +110,23 @@ const NavLink = styled.a`
   &:first-of-type {
     color: var(--color-secondary);
   }
-`;
+`
 
 const Filler = styled.div`
   flex: 1;
-`;
+`
 const Footer = styled.footer`
   flex: 1;
   display: flex;
   flex-direction: column;
   gap: 14px;
   justify-content: flex-end;
-`;
+`
 
 const SubLink = styled.a`
   color: var(--color-gray-700);
   font-size: 0.875rem;
   text-decoration: none;
-`;
+`
 
-export default MobileMenu;
+export default MobileMenu
